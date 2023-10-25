5b9458016354a7a3b4614c4af7e7f0fcfc33bb80
KeinoC <keino.chichester@gmail.com>
2023-10-24 23:28:28 -0400

    resume route setup

--- src/app/intro/intro.jsx
+++ src/app/intro/intro.jsx
@@ -4,8 +4,7 @@ import ThreeCube from "../globalComponents/three";
 export default function IntroPage() {
     return (
         <div className="container mx-auto">
-
-                <ThreeCube className='z-20 bg-red-500' />
+            
         </div>
     );
 }
---
+++ src/app/resume/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Resume() {
+    return (
+        <div>
+            {/* Yoeur resume content goes here */}
+        </div>
+    );
+};
+
+w
