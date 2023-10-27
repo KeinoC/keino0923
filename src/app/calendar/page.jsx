b9c1c218c9ae2490990adc9db5d4c169b83ba8d0
KeinoC <keino.chichester@gmail.com>
2023-10-26 23:48:53 -0400

    updated portfolio site calendar tab

---
+++ src/app/calendar/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page() {
+    return (
+        <div>
+            {/* Your content goes here */}
+        </div>
+    );
+}
+
+export default Page;
--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Resume() {
     return (
         <div>
-            {/* Yoeur resume content goes here */}
+            <h1>Resume</h1> 
         </div>
     );
 };
